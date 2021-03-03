7bf5324ba02f41d09073abdfee55b9a0173f8116
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-03-03 21:40:08 +0530

    Initial Commit

---
+++ ScreenTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTest
+//
+//  Created by Mahesh Prasad on 03/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
